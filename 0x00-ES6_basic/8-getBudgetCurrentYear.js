2cbb8fea801d6f299e22051f0b61275b74609ab3
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-06-28 03:39:54 -0400

    great

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,15 +4,11 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
-  const b1 = `income - ${getCurrentYear()}`;
-  const b2 = `gdp-${getCurrentYear()}`;
-  const b3 = `capita-${getCurrentYear()}`;
-
-  budget[b1] = income;
-  budget[b2] = gdp;
-  budget[b3] = capita;
+  const budget = {
+    [`income - ${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 
   return budget;
 }
