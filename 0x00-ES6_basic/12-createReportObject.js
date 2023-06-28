a629126296d8846d2fe3a631aacce8578dc56bed
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-06-28 11:46:27 -0400

    making progress

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,9 @@
 export default function createReportObject(employeesList) {
-    const objCreated = {
-        allEmployees: employeesList,
-        getNumberOfDepartments() {
-            let num = Object.keys(objCreated).length;
-            return num;
-        },
-    };
-    return objCreated;
+  const objCreated = {
+    allEmployees: employeesList,
+    getNumberOfDepartments() {
+      return (num = Object.keys(objCreated).length);
+    },
+  };
+  return objCreated;
 }
