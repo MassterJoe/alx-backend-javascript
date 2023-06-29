8eee048e75aaced4b29af6083455a9315cfbb4d1
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-06-29 03:53:24 -0400

    great

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,16 @@
+export default function handleResponseFromAPI(promise) {
+    return new Promise((resolve, reject) => {
+        if (promise) {
+            const obj = {
+                status: 200,
+                body: 'success'
+            }
+            resolve(obj);
+        } else {
+            reject(new Error({}));
+        }
+    })
+        .then((response) => {
+            console.log("Got a response from the API");
+        });
+}
