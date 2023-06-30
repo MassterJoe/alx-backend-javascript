6ed2677fac626122e34fcccf3b90258f65b16139
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-06-30 13:24:33 -0400

    go

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(fileName, lastName), uploadPhoto(fileName)])
-    .then((values) => values.map((value) => {
-      if (value.status === 'fulfilled') {
-        return {
-          status: value.status,
-          value: value.value,
-        };
-      }
-      return {
-        status: value.status,
-        reason: String(value.reason),
-      };
-    }));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => values.map((value) => ({
+    status: value.status,
+    value: value.status === 'fulfilled' ? value.value : String(value.reason),
+  })));
 }
