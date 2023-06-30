0234c58400e6333fa8fee5fbd477a0b4d4c52fa9
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-06-30 15:34:05 -0400

    awe

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,16 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function asyncUploadUser() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      const obj = {
-        photo: { status: `${photo.status}`, body: `${photo.body}` },
-        user: { firstName: `${user.firstName}`, lastName: `${user.lastName}` },
-      };
-      try {
-        return obj;
-      } catch (_) {
-        return { photo: null, user: null };
-      }
-    });
+export default async function asyncUploadUser() {
+  try {
+    return {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+  } catch (_) {
+    return { photo: null, user: null };
+  }
 }
