e18435b291272abd8fc73d3b5ea93a84e6f0bf31
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-06-30 14:54:11 -0400

    true

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -6,7 +6,7 @@ export default function guardrail(mathFunction) {
 
     queue.push(value);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push(x);
   }
