7fa2b2526f651c3ee4e092655ea87237f295e0dd
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-07-01 13:38:18 -0400

    praise God

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,51 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new Error('Amount must be a number');
+    } else if (!(currency instanceof Currency)) {
+      throw new Error('Currency must be an instance of Currency');
+    } else {
+      this._amount = amount;
+      this._currency = currency;
+    }
+  }
+
+  // getters and setters
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Value must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new Error('Currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._code} (${this._currency._name})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new Error('Amount must be a number');
+    } else if (typeof conversionRate !== 'number') {
+      throw new Error('ConversionRate must be a number');
+    } else {
+      return amount * conversionRate;
+    }
+  }
+}
