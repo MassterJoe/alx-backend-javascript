06707058c3f496476d688c5555b21f719fbb84f0
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-22 16:10:44 -0400

    great

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
 });
 rl.question('Welcome to Holberton School, what is your name?\n', (INPUT) => {
   console.log('Your name is: ', INPUT);
