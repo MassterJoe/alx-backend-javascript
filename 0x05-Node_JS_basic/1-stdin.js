3a7f18f73ecb1f659162e4193e2a426cc80cc1b4
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-22 17:34:56 -0400

    nice

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,6 +4,7 @@ process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString()}`);
   process.exit();
 });
+
 process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
