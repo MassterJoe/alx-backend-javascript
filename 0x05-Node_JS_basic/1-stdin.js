c685851e81e2b762a80786fcb5e35b203ee2d9ee
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-08-22 17:29:08 -0400

    cool

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,8 @@
-const readline = require('readline');
-
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.on('data', (data) => { 
+    process.stdout.write(`Your name is: ${data.toString()}`);
+        process.exit();
 });
-rl.question('Welcome to Holberton School, what is your name?\n', (input) => {
-  console.log('Your name is:', input);
-  console.log('This important software is now closing\n');
-  rl.close();
+process.on('exit', () => { 
+    process.stdout.write('This important software is now closing\n');
 });
